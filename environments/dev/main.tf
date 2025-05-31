d59315720c85b16d18b5229ee35267c7374bd776
Default User <chandurdissanayake0@gmail.com>
2025-06-01 01:17:00 +0530

    feat: added ngnix deployment and service

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -18,3 +18,42 @@ module "namespaces" {
     }
   ]
 }
+
+module "nginx-example-deployment" {
+  source = "../../terraform/modules/deployments"
+
+  deployments = [
+    {
+      manifest = "${path.module}/kubernetes/deployment.yaml"
+    }
+  ]
+}
+
+module "nginx-example-services" {
+  source = "../../terraform/modules/services"
+
+  services = [
+    {
+      manifest = "${path.module}/kubernetes/service.yaml"
+    }
+  ]
+}
+
+module "taints" {
+  source = "../../terraform/modules/taints"
+
+  taints = [
+    {
+      node_name = "minikube"
+      key       = "control-plane"
+      value     = "system"
+      effect    = "NoSchedule"
+    },
+    {
+      node_name = "minikube-m02"
+      key       = "apps"
+      value     = "nginx"
+      effect    = "NoSchedule"
+    }
+  ]
+}
--- terraform/modules/namespaces/output.tf
+++
@@ -1,4 +0,0 @@
-output "namespace_names" {
-  description = "List of namespace names created"
-  value       = [for ns in kubernetes_namespace.this : ns.metadata[0].name]
-}
